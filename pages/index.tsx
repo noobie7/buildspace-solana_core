73f99b11d00df21e28d2b53e8c54c4ae09e0f552
Mahendra Singh <mahendra@favtrail.com>
2023-03-23 11:03:48 +0530

    Completed : Reading data from blockchain

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,15 +4,57 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import AddressForm from '../components/AddressForm'
+import * as web3 from '@solana/web3.js'
 
 const Home: NextPage = () => {
   const [balance, setBalance] = useState(0)
   const [address, setAddress] = useState('')
+  const [isExecutable, setIsExecutable] = useState('')
 
-  const addressSubmittedHandler = (address: string) => {
-    setAddress(address)
-    setBalance(1000)
+  const addressSubmittedHandler = async (address: string) => {
+    try{
+      const key = new web3.PublicKey(address);
+      setAddress(key.toBase58());
+      const connection = new web3.Connection(web3.clusterApiUrl('devnet'))
+      connection.getBalance(key).then((balance) => {
+       setBalance(balance / web3.LAMPORTS_PER_SOL);
+      })
+      await getAccountInfoBalanceUsingJSONRPC(address);
+    } catch(error){
+      setAddress('')
+      setBalance(0)
+      alert(error)
+    }
   }
+
+  async function getAccountInfoBalanceUsingJSONRPC(address: string): Promise<void> {
+    const url = web3.clusterApiUrl('devnet')
+    console.log(url);
+    return fetch(url, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+            "jsonrpc": "2.0",
+            "id": 1,
+            "method": "getAccountInfo",
+            "params": [
+                address
+            ]
+        })
+    }).then(response => response.json())
+    .then(json => {
+        if (json.error) {
+            throw json.error
+        }
+        if(json['result']['value']['executable'] === true)
+          setIsExecutable('Yay!!!');
+        else 
+          setIsExecutable('Nope!');
+    })
+    .catch(error => {
+        throw error
+    })
+}
 
   return (
     <div className={styles.App}>
@@ -23,6 +65,7 @@ const Home: NextPage = () => {
         <AddressForm handler={addressSubmittedHandler} />
         <p>{`Address: ${address}`}</p>
         <p>{`Balance: ${balance} SOL`}</p>
+        <p>{`is Executable : ${isExecutable}`}</p>
       </header>
     </div>
   )
